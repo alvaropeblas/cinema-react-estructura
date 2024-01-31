672ff22f5b1152989037206db25e48778d4c9802
alvaropeblas <121167838+alvaropeblas@users.noreply.github.com>
2024-01-31 20:23:05 +0100

    searchCompleted

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -13,27 +13,7 @@ const linkClass = 'block py-2 px-3 text-white rounded hover:text-primary-600 md:
 
 let timeoutId
 function AppNavbar() {
-    const [searchTerm, setSearchTerm] = useState();
-    const [searchResult, setSearchResult] = useState([]);
-    const [showResults, setShowResults] = useState(false);
-
-
-    async function handleAnimeSearch() {
-        clearTimeout(timeoutId);
-        const input = document.querySelector('input[type="search"]');
-        const newSearchTerm = input.value;
-        setSearchTerm((search) => newSearchTerm);
-        timeoutId = setTimeout(async () => {
-            const search = await getAnimeByName(newSearchTerm);
-            setSearchResult(search);
-            setShowResults(true);
-        }, 600);
-    }
-
-    const clearSearchResults = () => {
-        setSearchResult([]);
-        setShowResults(false);
-    };
+    
 
     return (
         <nav className="bg-darksurf-200 rounded-md mb-2">
@@ -64,33 +44,7 @@ function AppNavbar() {
                         }
                     </ul>
                 </div>
-                <div>
-                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </div>
-                        <input type="search" /* onBlur={clearSearchResults} */ onKeyUp={handleAnimeSearch} id="default-search" className="block w-[350px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search animes..." required />
-                        <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-primary-100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-100 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-                        {showResults && (
-                            <div className="absolute top-full left-0 z-10 bg-white border border-gray-300 mt-2 w-[350px] rounded-lg overflow-hidden shadow-lg">
-                                {searchResult.length > 0 ? (
-                                    <ul className="divide-y divide-gray-300">
-                                        {searchResult.map((result, index) => (
-                                            <Link key={result.mal_id} to={link.path + "/" + result.mal_id} className="p-3">
-                                                <p className=" text-black hover:text-primary-100">{result.title}</p>
-                                            </Link>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <p className="p-3">No se encontraron resultados.</p>
-                                )}
-                            </div>
-                        )}
-                    </div>
-                </div>
+               
             </div>
         </nav >
     );
--- src/pages/Films/Films.jsx
+++ src/pages/Films/Films.jsx
@@ -1,14 +1,39 @@
 import { useEffect, useState } from "react";
-import { getAllAnimes, getAmimeGenres } from "../../services/films";
+import { getAllAnimes, getAmimeGenres, getAnimeByName } from "../../services/films";
 import { Card, Pagination } from "flowbite-react";
 import ghost from "../../../public/images/ghost.png";
-import { NavLink } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+let timeoutId
 function Films() {
   const [isLoading, setIsLoading] = useState(true)
   const [films, setFilms] = useState([]);
   const [genres, setGenres] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState();
+  const [searchResult, setSearchResult] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+
+
+  async function handleAnimeSearch() {
+    clearTimeout(timeoutId);
+    const input = document.querySelector('input[type="search"]');
+    const newSearchTerm = input.value;
+    setSearchTerm((search) => newSearchTerm);
+    timeoutId = setTimeout(async () => {
+      const search = await getAnimeByName(newSearchTerm);
+      setSearchResult(search);
+      setShowResults(true);
+      setFilms(search)
+    }, 300);
+  }
+
+  useEffect(() => {
+    fetchAnimeGenres().then((data) => {
+      setGenres(data);
+    });
+  }, []);
+
+
   const link = { name: "FilmDetails", path: "/filmDetails" }
   async function fetchAnime() {
     try {
@@ -50,11 +75,8 @@ function Films() {
     }).finally(() => setIsLoading(false));
   }, [currentPage]);
 
-  useEffect(() => {
-    fetchAnimeGenres().then((data) => {
-      setGenres(data);
-    });
-  }, []);
+
+
   return (
     <section className="mt-16">
       <div className="absolute left-50">
@@ -68,7 +90,17 @@ function Films() {
       <h1 className="font-rubiksh text-gray-200 font-extrabold text-4xl mb-4">
         Animes en cartelera
       </h1>
-
+      <div className="absolute right-[14%] top-[20%]">
+        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input type="search"  onKeyUp={handleAnimeSearch} id="default-search" className="block w-[350px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search animes..." required />
+        </div>
+      </div>
       <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-3 mt-10">
         {isLoading ? (
           Array.from({ length: 10 }).map((_, index) => (
